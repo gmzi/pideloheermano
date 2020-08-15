9ed743eef5a0764a058d36eaf00c9d49b230cd2f
gmzi <gastonmieres@hotmail.com>
2020-08-15 12:38:31 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const response = document.querySelector(".aleluya");
+const frases = [
+  "aleluya!!!",
+  "gloria!!!!",
+  "recíbelo!!!",
+  "siéntelo",
+  "alábalo!!!",
+  "ya viene",
+];
+
+// Event listeners
+todoButton.addEventListener("click", addTodo);
+
+// Functions
+function addTodo(event) {
+  // prevent form from submitting
+  event.preventDefault();
+  // Select esponse after prayer:
+  const alaba = frases[Math.floor(Math.random() * frases.length)];
+  // Display response after prayer:
+  response.innerText = alaba;
+  //   clear  input value:
+  todoInput.value = "";
+}
